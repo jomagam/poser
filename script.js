40069dcddba60733b1b69e6fd1efabeefe7bed1d
Balazs Rauznitz <jomagam@yahoo.com>
2013-01-10 23:26:36 -0700

    added TODO; plus cleanup

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var got_images = 0;
 chrome.extension.onRequest.addListener(function(request) {
     if(request.sorry){
-// alert("GOT SORRY");
 	$('#imageinfo').html(request.sorry);
     }
     else {
-// alert("GOT SRC=" + request.src + " WIDTH=" + request.width);
 	got_images++;
 	$('#imageinfo').html($('#imageinfo').html() + request.src + " " + request.width + "px #" + got_images + "<br>");
     }
